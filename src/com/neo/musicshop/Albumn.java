72bd7641d437b5a6682a40364eae284232f32e2f
Ovie Umukoro <ovie.u.umukoro@gmail.com>
2018-01-23 19:50:22 -0500

    Add albumn class

--- src/com/neo/musicshop/Albumn.java
+++ src/com/neo/musicshop/Albumn.java
@@ -1,4 +1,48 @@
 package com.neo.musicshop;
+import java.util.*;
 
 public class Albumn {
+
+    /** Constructor
+     *  Note that the albumn name and band names are
+     *  immutable once the albumn is created
+     */
+    public Albumn(String albumnName, String bandName) {
+        title = albumnName;
+        band = bandName;
+    }
+
+    public String bandName() {
+        return band;
+    }
+
+    public String albumnName() {
+        return title;
+    }
+
+    /** Adds a song to this albumn. There is no duplicate
+     *  checking for songs that are added.
+     */
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    /** Returns a string representation of an album listing
+     *  the albumn name and the band name
+     */
+    public Iterator<Song> getSongs() {
+        return songs.iterator();
+    }
+
+    /** Returns a string representation of the albumn
+     *  listing, the albumn name and the band name
+     */
+    public String toString() {
+        return ("\"" + title + "\"by" + band);
+    }
+
+    /** Private instance variables */
+    private String title;
+    private String band;
+    private ArrayList<Song> songs = new ArrayList<Song>();
 }
